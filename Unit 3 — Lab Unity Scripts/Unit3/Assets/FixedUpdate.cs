1395387aaa8e421af5d1ba68460425fa85feb0b0
Jordan <jordanmears23@gmail.com>
2023-02-15 16:22:09 -0700

    FixedUpdate

---
+++ Unit 3 — Lab Unity Scripts/Unit3/Assets/FixedUpdate.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class FixedUpdate : MonoBehaviour
+{
+    void FixedUpdate ()
+    {
+        Debug.Log("FixedUpdate time :" + Time.deltaTime);
+    }
+    
+    
+    void Update ()
+    {
+        Debug.Log("Update time :" + Time.deltaTime);
+    }
+}
