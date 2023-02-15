423d050463043627dedbb2a3056eb7f043c6745d
Jordan <jordanmears23@gmail.com>
2023-02-15 15:45:44 -0700

    VarsFunctions Modify

--- Unity 2 — Lab Submit GIT with Unity Project/DGM1610_Spring2023/Assets/VariablesFunctions.cs
+++ Unity 2 — Lab Submit GIT with Unity Project/DGM1610_Spring2023/Assets/VariablesFunctions.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class VariablesFunctions : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{   
+    int myInt = 5;
+    
+    
+    void Start ()
     {
-        
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log (myInt);
     }
-
-    // Update is called once per frame
-    void Update()
+    
+    
+    int MultiplyByTwo (int number)
     {
-        
+        int result;
+        result = number * 2;
+        return result;
     }
 }
