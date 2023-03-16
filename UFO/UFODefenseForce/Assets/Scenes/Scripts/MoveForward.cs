c9f49c8205461fbb3159a9eb3339d6b471457431
Jordan <jordanmears23@gmail.com>
2023-03-15 23:35:55 -0600

    UFO game

---
+++ UFO/UFODefenseForce/Assets/Scenes/Scripts/MoveForward.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float speed = 50.0f;
+    
+    void Update()
+    {
+        //move game object forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
---
+++ UFO/UFODefenseForce/Assets/Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 25;
+    public float xRange = 33;
+
+    public Transform blaster;
+    public GameObject lazerBolt;
+    
+    void Update()
+    {
+        //Set horizontalInput to recieve values from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+        //moves player left and right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+       //keep player within bounds leftside
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        //keep player within bounds rightside
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        //if spacebar is pressed fire lazerbolt
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //create lazerbolt at the blaster transform position maintaining the object rotation
+            Instantiate(lazerBolt, blaster.transform.position, lazerBolt.transform.rotation);
+        }
+    }
+    //Delte any object with a trigger that hits the player
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
